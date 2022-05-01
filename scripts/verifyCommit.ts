afbc4b3c39a534a70aab4e88cb6a3971358e26f2
Victor Hugo Castro Contreras <vcastroc@TRJ-C02GL0VXQ05P.local>
2022-04-30 22:15:10 -0500

    CBK-11: Update VerifyCommit

--- scripts/verifyCommit.ts
+++ scripts/verifyCommit.ts
@@ -5,11 +5,9 @@ const msgPath = process.env.GIT_PARAMS
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const releaseRE = /^v\d/
-const commitRE =
-  /^(revert: )?(CBK-|feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(CBK-)(\(.+\))?[0-9]{0,999}: .{1,50}/
 
 if (!releaseRE.test(msg) && !commitRE.test(msg)) {
-  console.log(msg)
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
       `invalid commit message format.`
@@ -17,8 +15,8 @@ if (!releaseRE.test(msg) && !commitRE.test(msg)) {
       chalk.red(
         `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
       ) +
-      `    ${chalk.green(`feat: add 'comments' option`)}\n` +
-      `    ${chalk.green(`fix: handle events on blur (close #28)`)}\n\n` +
+      `    ${chalk.green(`CBK-40: Actualizacion del home`)}\n` +
+      `    ${chalk.green(`CBK-40: handle events on blur (close #28)`)}\n\n` +
       chalk.red(`  See .github/commit-convention.md for more details.\n`)
   )
   process.exit(1)
