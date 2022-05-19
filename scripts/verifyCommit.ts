35ceffa3e0c9aa9691b0670270c566984deaf38a
ElvisTaco <etaco@gruporoi.com>
2022-05-19 18:15:38 -0500

    WCX-11: Update verifyCommit to new format

--- scripts/verifyCommit.ts
+++ scripts/verifyCommit.ts
@@ -5,7 +5,7 @@ const msgPath = process.env.GIT_PARAMS
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const releaseRE = /^v\d/
-const commitRE = /^(revert: )?(CBK-)(\(.+\))?[0-9]{0,999}: .{1,50}/
+const commitRE = /^(revert: )?(WCX-)(\(.+\))?[0-9]{0,999}: .{1,50}/
 
 if (!releaseRE.test(msg) && !commitRE.test(msg)) {
   console.error(
@@ -15,8 +15,8 @@ if (!releaseRE.test(msg) && !commitRE.test(msg)) {
       chalk.red(
         `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
       ) +
-      `    ${chalk.green(`CBK-40: Actualizacion del home`)}\n` +
-      `    ${chalk.green(`CBK-40: handle events on blur (close #28)`)}\n\n` +
+      `    ${chalk.green(`WCX-40: Actualizacion del home`)}\n` +
+      `    ${chalk.green(`WCX-40: handle events on blur (close #28)`)}\n\n` +
       chalk.red(`  See .github/commit-convention.md for more details.\n`)
   )
   process.exit(1)
